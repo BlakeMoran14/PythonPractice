511fd228dfe24297dcef1f968f55a9723f087a20
BlakeMoran14 <818523@mystma.org>
2021-09-13 15:10:33 -0500

    Made first main python file.

---
+++ main.py
@@ -0,0 +1,15 @@
+# Start of python file.
+
+# Main Code
+print("Hello World!")
+print("This is a testing python file...")
+print("Goodbye World!")
+
+# Print first movie
+print('Maze Runner')
+
+# Print second movie
+print('Divergent')
+
+# Print third movie
+print('The Giver')
