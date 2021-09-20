dfdb6aded441d7ad8f18b48769a2ec87b0bdd125
BlakeMoran14 <818523@mystma.org>
2021-09-20 14:23:22 -0500

    todays work

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
-"""import time
-import os
-
-def newLine():
-    print("")
-
-def topFiveMovies():
-    # First Movie
-    print("ThE LoREd Of THEWA FLWEIS")
-
-    # Second Movie
-    print("ANother movie")
-
-    # Third Movie
-    print("ANother movie lol")
-
-def topFiveBooks():
-    print("Maze Runner")
-    print("One of us is lying")
-    print("The Giver")
-
-def printHello():
-    print("Hello")
-
-def printTest():
-    print("Test")
-
-def echoTest():
-    os.system("echo This is a CMD ECHO")
-
-topFiveMovies()
-newLine()
-topFiveBooks()
-newLine()
-printHello()
-newLine()
-printTest()
-newLine()
-echoTest()"""
-
 def myNameIs(name):
     print("My name is " + name)
 
-myNameIs("blake")
+def timesTen(num):
+    print(num * 10)
+
+def plusFive(num):
+    print(num + 5)
+
+def divByTwo(num):
+    print(num / 2)
+
+def addTwoNumber(num, num2):
+    print(num + num2)
+
+def storyTest(name, age, grade):
+    print(name + " is " + str(age) + " years old, and in " + str(grade) + "th grade.")
+
+def madlib(instrument, number, day, letter, key, number1, number2, adjective, adjective1, fraction, adjective2, instrument1, adjective3, noun, familymember, nameofsong, adjectiveEd, favoriteArtist):
+    print("While I've stayed at home, I've practiced the " + instrument + " for " + number + " hours  every " + day + ". My favorite key signature to play and practice in is " + letter + key + ". It has " + number1 + " flats and " + number2 + " sharps." + " I like this key signature because it is " + adjective + " and " + adjective1 + " I've also gotten awesome at counting time signatures. The time signature I love is " + fraction + ", because it is " + adjective2 + " to count. One instrument I want to learn how to play while I'm at home is " + instrument1 + ", because it is " + adjective3 + " and sounds like a " + noun + ". My " + familymember + " likes it when I play " + nameofsong + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + adjectiveEd + " of how great I am at playing my instrument. My teacher might even think I sound like " + favoriteArtist + "!")
+
+
+madlib("Trombone", "61", "week", "a", "minor", "1", "2", "saucy", "crusty", "1/2", "fatly", "symbols", "knarly", "car", "Crustacia", "Goin Baby by DaBaby in Baby on Baby's Album", "crustied", "DaBaby")
+
