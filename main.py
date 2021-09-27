c2345df7914b4e22d8cffdd0c71486f27668d8f6
BlakeMoran14 <818523@mystma.org>
2021-09-27 14:13:51 -0500

    testing

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-def color():
-    return "red"
+def greaterThan10(x):
+    if (x > 10):
+        return True
+    if (x < 10):
+        return False
+    else:
+        return "The input was: 10"
 
-print(color() + " is my favorite color!")
+print(greaterThan10(10))
+
+def potato(x, y):
+    if (x + y) > 10:
+        return "potato"
+    if (x + y) == 10:
+        return "tomato"
+    if (x + y) < 10:
+        return "topato"
