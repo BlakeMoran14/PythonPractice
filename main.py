01e3429a1df8f01486c04cef3cc7ac78ac17886f
BlakeMoran14 <818523@mystma.org>
2021-10-05 14:22:56 -0500

    stuff

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-def greaterThan10(x):
-    if (x > 10):
-        return True
-    if (x < 10):
-        return False
-    else:
-        return "The input was: 10"
+def spellingBee(w):
+    for i, in w:
+        print(i)
 
-print(greaterThan10(10))
-
-def potato(x, y):
-    if (x + y) > 10:
-        return "potato"
-    if (x + y) == 10:
-        return "tomato"
-    if (x + y) < 10:
-        return "topato"
+spellingBee("Hello")
